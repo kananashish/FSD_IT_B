898b6723b0940a064160207c40df8039e4c75153
kananashish <ashishkananconnect@gmail.com>
2025-02-19 15:41:58 +0530

    backend added

---
+++ Backend/day2/MyMath.js
@@ -0,0 +1,13 @@
+let sum = (a,b) => a+b;
+let mul = (a,b) => a*b;
+let div = (a,b) => a/b;
+
+//module.exports = {sum,mul,div};
+
+const obj = {
+    sum,
+    mul,
+    div
+}
+
+module.exports = obj;
